a47e76852c28fb0fc6d80758902ea16ac3046340
SalxIeva <ieva.saldukaite@gmail.com>
2024-08-08 15:50:31 +0100

    click event added to the submit button, if passwords do not match to let user know

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
 // console.log('Hello');
+
+    const password = document.querySelector("#password");
+    
+    const confirm = document.querySelector("#confirm-password");
+    
+    const submit = document.querySelector("#submitBtn");
+    
+    submit.addEventListener("click", () => {
+    if (password.value === confirm.value) {
+    confirm.setCustomValidity("");
+} else {
+    confirm.setCustomValidity("Passwords do not match");
+};
+
+});
